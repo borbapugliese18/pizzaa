5a7c72cf4468f622cbce6790fcc8b9cfc15a2588
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-28 11:41:53 -0300

    aaaa

---
+++ app/src/main/java/br/com/gabrielborba/kawabangapizzas/model/Pedido.kt
@@ -0,0 +1,13 @@
+package br.com.gabrielborba.kawabangapizzas.model
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+
+@Parcelize
+data class Pedido(
+    val nomeCliente: String,
+    val sabores: List<String>,
+    val tamanho: String,
+    val tipoPagamento: String
+) : Parcelable
